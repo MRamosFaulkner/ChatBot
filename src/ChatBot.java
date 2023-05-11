8df5b5ac1d5e2488729c4f7962245a41539f664b
unknown <dnal03@msn.com>
2023-05-10 21:48:37 -0400

    Chat Bot

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,17 +1,74 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+public class ChatBot {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        printGreeting();
+        repeatName();
+        guessAge();
+        numberCount();
+        knowledge();
+    }
+    public static String printGreeting() {
+        System.out.println("Hello! I'm Chatty. What's your name? ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String nameEntered = scanner.nextLine();
+        return nameEntered;
+    }
+    public static String repeatName() {
+        System.out.println("Sorry, I seem to have had a glitch. Can you repeat your name, please?");
+
+        String name2 = scanner.nextLine();
+        System.out.println("Thanks " + name2 + ". I would like to play a game and guess your age!\n");
+        return name2;
+    }
+    public static void guessAge() {
+        System.out.println("What kind of music do you like? 80's Rock, 70's Disco, 0r 90's Pop?");
+
+        String musicType = scanner.nextLine();
+
+        int age;
+
+        if (musicType.equalsIgnoreCase("80's Rock")) {
+            age = 32;
+        } else if (musicType.equalsIgnoreCase("70's Disco")) {
+            age = 65;
+        }else {
+            age = 28;
+        }
+            System.out.println("I'm going to say you are " + age + " years old." );
+    }
+    public static void numberCount() {
+        System.out.println("Okay! Let's try something else. Pick a number and I will count to it.");
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if(num > 0) {
+            for(int i = 1; i <= num; i++) {
+                System.out.println(i);
+            }
+        } else if(num < 0) {
+            for (int i = -1; i >= num; i--) {
+                System.out.println(i);
+            }
         }
     }
+    public static void knowledge () {
+        System.out.println("Let's test your programming knowledge");
+        System.out.println("In Java, if I want to show decimals. What do I use? Choose from the following options?");
+        System.out.println("A) int");
+        System.out.println("B) dec");
+        System.out.println("C) double");
+        System.out.println("D) decimal");
+
+        String answer = scanner.nextLine();
+
+        if (answer.equals("C) double")) {
+            System.out.println("Congrats! You know your stuff!");
+        } else {
+            System.out.println("Sorry! Go hit the books and learn some more!");
+        }
+    }
+
 }
+
